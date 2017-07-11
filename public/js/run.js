79c984d2b2c0b38c11b9dbbd194ed5472f466053
Dimitar <dimdimvas@yahoo.com>
2017-07-10 18:11:44 -0700

    Main functionality completed. Time to start styling. Yaml imported as CSS framework - not sure if it is going to be used

--- public/js/run.js
+++ public/js/run.js
@@ -1,105 +1,5 @@
-var map;    
-
-function initMap() {
-
-    // Create a styles array to use with the map.
-    var styles = [
-        {
-            featureType: 'water',
-            stylers: [
-                { color: '#19a0d8' }
-            ]
-        },{
-            featureType: 'administrative',
-            elementType: 'labels.text.stroke',
-            stylers: [
-                { color: '#ffffff' },
-                { weight: 6 }
-            ]
-        },{
-            featureType: 'administrative',
-            elementType: 'labels.text.fill',
-            stylers: [
-                { color: '#e85113' }
-            ]
-        },{
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [
-                { color: '#efe9e4' },
-                { lightness: -40 }
-            ]
-        },{
-            featureType: 'transit.station',
-            stylers: [
-                { weight: 9 },
-                { hue: '#e85113' }
-            ]
-        },{
-            featureType: 'road.highway',
-            elementType: 'labels.icon',
-            stylers: [
-                { visibility: 'off' }
-            ]
-        },{
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [
-                { lightness: 100 }
-            ]
-        },{
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [
-                { lightness: -100 }
-            ]
-        },{
-            featureType: 'poi',
-            elementType: 'geometry',
-            stylers: [
-                { visibility: 'on' },
-                { color: '#f0e4d3' }
-            ]
-        },{
-            featureType: 'road.highway',
-            elementType: 'geometry.fill',
-            stylers: [
-                { color: '#efe9e4' },
-                { lightness: -25 }
-            ]
-        }
-    ];
-
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.9779776, lng: -122.0310733},
-        zoom: 13,
-        styles: styles,
-        mapTypeControl: false
-    });
-
-
-    // This autocomplete is for use in the geocoder entry box.
-    var zoomAutocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('zoom-to-area-text'));
-    
-    // Bias the boundaries within the map for the zoom to area text.
-    zoomAutocomplete.bindTo('bounds', map);
-
-     // Create a searchbox in order to execute a places search
-    var searchBox = new google.maps.places.SearchBox(
-        document.getElementById('places-search'));
-    
-    // Bias the searchbox to within the bounds of the map.
-    searchBox.setBounds(map.getBounds());
-
-        // Listen for the event fired when the user selects a prediction from the
-    // picklist and retrieve more details for that place.
-    searchBox.addListener('places_changed', function() {
-        searchBoxPlaces(this);
-    });
-
-}
+var map;
+var placeInfoWindow;
 
 var ViewModel = function(){
 
@@ -114,6 +14,14 @@ var ViewModel = function(){
     self.textSearch = function(){
         textSearchPlaces();
     };
+
+    self.autoSearch = function(searchBox){
+        searchBoxPlaces(searchBox);
+    }
+
+    self.getMarkerInfo = function(marker){
+        getPlacesDetails(marker, placeInfoWindow);
+    }
 
     // This function will loop through the listings and hide them all.
     function hideMarkers(markers) {
@@ -216,7 +124,7 @@ var ViewModel = function(){
 
             // Create a single infowwindow to be used with the place details information
             // so that only one is open at once.
-            var placeInfoWindow = new google.maps.InfoWindow();
+            
             // If a marker is clicked, do a place details search on it in the next function.
             marker.addListener('click', function(){
                 if(placeInfoWindow.marker == this){
@@ -225,7 +133,7 @@ var ViewModel = function(){
                     getPlacesDetails(this, placeInfoWindow);
                 }
             });
-            
+
             self.placeMarkers.push(marker);
 
             if(place.geometry.viewport){
@@ -368,4 +276,109 @@ var ViewModel = function(){
 
 }
 
-ko.applyBindings(new ViewModel());
+var viewModel = new ViewModel();
+ko.applyBindings(viewModel);
+
+
+function initMap() {
+
+    placeInfoWindow = new google.maps.InfoWindow();
+
+    // Create a styles array to use with the map.
+    var styles = [
+        {
+            featureType: 'water',
+            stylers: [
+                { color: '#19a0d8' }
+            ]
+        },{
+            featureType: 'administrative',
+            elementType: 'labels.text.stroke',
+            stylers: [
+                { color: '#ffffff' },
+                { weight: 6 }
+            ]
+        },{
+            featureType: 'administrative',
+            elementType: 'labels.text.fill',
+            stylers: [
+                { color: '#e85113' }
+            ]
+        },{
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [
+                { color: '#efe9e4' },
+                { lightness: -40 }
+            ]
+        },{
+            featureType: 'transit.station',
+            stylers: [
+                { weight: 9 },
+                { hue: '#e85113' }
+            ]
+        },{
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [
+                { visibility: 'off' }
+            ]
+        },{
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [
+                { lightness: 100 }
+            ]
+        },{
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [
+                { lightness: -100 }
+            ]
+        },{
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [
+                { visibility: 'on' },
+                { color: '#f0e4d3' }
+            ]
+        },{
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [
+                { color: '#efe9e4' },
+                { lightness: -25 }
+            ]
+        }
+    ];
+
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.9779776, lng: -122.0310733},
+        zoom: 13,
+        styles: styles,
+        mapTypeControl: false
+    });
+
+
+    // This autocomplete is for use in the geocoder entry box.
+    var zoomAutocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('zoom-to-area-text'));
+    
+    // Bias the boundaries within the map for the zoom to area text.
+    zoomAutocomplete.bindTo('bounds', map);
+
+     // Create a searchbox in order to execute a places search
+    var searchBox = new google.maps.places.SearchBox(
+        document.getElementById('places-search'));
+    
+    // Bias the searchbox to within the bounds of the map.
+    searchBox.setBounds(map.getBounds());
+
+        // Listen for the event fired when the user selects a prediction from the
+    // picklist and retrieve more details for that place.
+    searchBox.addListener('places_changed', function() {
+        viewModel.autoSearch(searchBox);
+    });
+
+}
